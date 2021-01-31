3922f07643a7a6c02342a4208fdce01f47aadfbd
Edmand Looi <edmandlooi@gmail.com>
2021-01-31 01:01:03 -0600

    End of section 8

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }  // Automapper will automatically find/use the GetAge() to compute the Age using the convension Get<property name>();
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
