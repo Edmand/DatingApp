847d8cabc9bd9bd9b3f8c9bbf23daabbf1ad78c0
Edmand Looi <edmandlooi@gmail.com>
2021-01-30 18:10:24 -0600

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -24,9 +24,6 @@ export class NavComponent implements OnInit {
 
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     });
   }
 
